7d3a8810353c0f6b3eb85fae158a4e1f40ae01b0
ayush singh <ayushs24101@iiitnr.edu.in>
2025-06-29 02:09:48 +0530

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/docs", // backend running on 5000
+  baseURL: "https://codenarrator.onrender.com/api/docs", // backend running on 5000
 });
 
 export default API;
